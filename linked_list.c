1c1a81812d1719021e153add9374046674ae2845
Toni Lammi <toni.lammi@student.tut.fi>
2018-06-13 22:04:00 +0300

    Added a simple implementation of a linked list

---
+++ linked_list.c
@@ -0,0 +1,86 @@
+#include "linked_list.h"
+
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+struct LinkedList LinkedList_new(void){
+  struct LinkedList l = {
+    .head = NULL,
+    .current = NULL,
+    .tail = NULL
+  };
+  return l;
+}
+
+void LinkedList_destroy(struct LinkedList* handl){
+  if(handl == NULL) return;
+  struct LinkedNode* ptr = handl->head;
+  while(ptr->next != NULL){
+    ptr = ptr->next;
+    free(ptr->prev);
+  }
+  free(ptr);
+}
+
+void LinkedList_pushBack(struct LinkedList* handl, const void* val, int bytes){
+
+
+  struct LinkedNode* ptr = malloc(sizeof(struct LinkedNode)+bytes);
+  memcpy(&ptr->val, val, bytes);
+  ptr->bytes = bytes;
+  ptr->next = NULL;
+  if(handl->tail != NULL){
+    handl->tail->next = ptr;
+    ptr->prev = handl->tail;
+    handl->tail = ptr;
+  } else {
+    ptr->prev = NULL;
+    handl->head = ptr;
+    handl->tail = ptr;
+    handl->current = ptr;
+  }
+}
+
+void LinkedList_pushFront(struct LinkedList* handl, const void* val, int bytes){
+  struct LinkedNode* ptr = malloc(sizeof(struct LinkedNode)+bytes);
+  memcpy(ptr->val, val, bytes);
+  ptr->bytes = bytes;
+  ptr->prev = NULL;
+
+  if(handl->head != NULL){
+    handl->head->prev = ptr;
+    ptr->next = handl->head;
+    handl->head = ptr;
+  } else {
+    ptr->next = NULL;
+    handl->head = ptr;
+    handl->tail = ptr;
+    handl->current = ptr;
+  }
+
+}
+
+int LinkedList_getNext(struct LinkedList* handl, void** val, int* bytes){
+  if(handl->current == handl->tail) return -1;
+  handl->current = handl->current->next;
+  *val = &handl->current->val;
+  *bytes = handl->current->bytes;
+  return 0;
+}
+int LinkedList_getPrev(struct LinkedList* handl, void** val, int* bytes){
+  if(handl->current == handl->head) return -1;
+  handl->current = handl->current->prev;
+  *val = &handl->current->val;
+  *bytes = handl->current->bytes;
+  return 0;
+}
+
+
+
+int LinkedList_getCurr(struct LinkedList* handl, void** val, int* bytes){
+  if(handl->current == NULL) return -1;
+  *val = &handl->current->val;
+  *bytes = handl->current->bytes;
+  return 0;
+}
---
+++ linked_list.h
@@ -0,0 +1,28 @@
+#ifndef LINKED_LIST_H
+#define LINKED_LIST_H
+
+struct LinkedNode {
+  struct LinkedNode* next;
+  struct LinkedNode* prev;
+  int bytes;
+  int val[0];
+};
+
+struct LinkedList {
+  struct LinkedNode* head;
+  struct LinkedNode* current;
+  struct LinkedNode* tail;
+};
+
+struct LinkedList LinkedList_new(void);
+void LinkedList_destroy(struct LinkedList* handl);
+
+void LinkedList_pushBack(struct LinkedList* handl, const void* val, int bytes);
+
+void LinkedList_pushFront(struct LinkedList* handl, const void* val, int bytes);
+
+int LinkedList_getNext(struct LinkedList* handl, void** val, int* bytes);
+int LinkedList_getPrev(struct LinkedList* handl, void** val, int* bytes);
+int LinkedList_getCurr(struct LinkedList* handl, void** val, int* bytes);
+
+#endif
